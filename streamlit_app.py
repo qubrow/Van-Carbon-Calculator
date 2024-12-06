6b99c807805dd0e34cb94a305f3311fea620bc50
qubrow <qubr4676@colorado.edu>
2024-12-06 14:00:43 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ def main():
     fuel_efficiency = st.number_input("Enter Fuel Efficiency (MPG)", min_value=10.0, step=1.0)
     st.info("An average van will get 17.8 mpg according to the US Department of Energy, although this will depend on brand, wear, and maintenance.")
     has_cartop_carrier = st.checkbox("Do you have a cartop carrier?")
-    added_weight = st.number_input("Enter Added Weight (in pounds)", help: "-Cabinets and storage units: 50–150 lbs/n-Table and seating: 40–100 lbs/n", min_value=0, step=10)
+    added_weight = st.number_input("Enter Added Weight (in pounds)", help="-Cabinets and storage units: 50–150 lbs/n-Table and seating: 40–100 lbs/n", min_value=0, step=10)
     st.info("CAT scales, typically found at truck stops, can tell you exactly how much your van weighs. Going over your van's GVWR (Gross Vehicle Weight Rating) can result in vehicle damage")
     st.info("A minimalist setup might add around 500–800 lbs.")
     st.info("A larger build out could add 1500 to 2500+ lbs.")
